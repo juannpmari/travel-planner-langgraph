8fbdc24d6396419cf9f01aa4a615999fdf5470d7
juannpmari <juannpmari@gmail.com>
2024-12-07 10:45:23 -0300

    Add fake acommodation data, simulating a travel agency

--- tools/services_tools/acommodation_data.py
+++ tools/services_tools/acommodation_data.py
@@ -1,8 +1,14 @@
 from langchain_core.tools import tool
+from pydantic import BaseModel
+import pandas as pd
+
+acommodation_df = pd.read_csv('data/acommodation_data.csv')
 
 @tool
-def get_acommodation_data():
+def get_acommodation_data(query:str):
     """
     Call to get data about acommodation services offered by our company
+    Args:
+        query: pandas' dataframe formatted query, to filter acommodation data according to user needs. For example, query = "city in ['London','Paris']"
     """
-    return "For Loreto, we've a 5 stars hotel at 100USD per night. we also have many hotels all over the world, just call us for updated information"
+    return acommodation_df.query(query)
